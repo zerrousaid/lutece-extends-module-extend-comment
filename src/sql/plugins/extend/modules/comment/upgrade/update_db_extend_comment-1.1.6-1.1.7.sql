57ba241ecc74701ee66db1a50f109942aa4b778c
FrancoisEricMerlin <francoiseric.merlin@paris.fr>
2016-02-29 11:52:04 +0100

    EXTENDCOMM-49:-add in front office the possibility to sort comment on creation date

--- src/sql/plugins/extend/modules/comment/upgrade/update_db_extend_comment-1.1.6-1.1.7.sql
+++ src/sql/plugins/extend/modules/comment/upgrade/update_db_extend_comment-1.1.6-1.1.7.sql
@@ -1,8 +1,4 @@
---
--- EXTENDCOMMENT- : Add display sub comments mod
---
-ALTER TABLE extend_comment_config ADD COLUMN is_enabled_display_sub_comments SMALLINT default 0 NOT NULL;
-ALTER TABLE extend_comment_config ADD COLUMN is_enabled_delete_comments SMALLINT default 0 NOT NULL;
+
 ALTER TABLE extend_comment_config ADD COLUMN is_comments_sorted_by_date_creation SMALLINT default 0 NOT NULL;
 
 
